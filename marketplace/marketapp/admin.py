f655eca7549bf3db33df68baa814f7089da47373
bava-kurian <bavakurian135@gmail.com>
2024-02-25 20:49:20 +0530

    commit 2
    added models
    but has some errors

--- marketplace/marketapp/admin.py
+++ marketplace/marketapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.CropType)
+admin.site.register(models.Farmer)
+admin.site.register(models.Product)
+admin.site.register(models.Order)
--- marketplace/marketapp/models.py
+++ marketplace/marketapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+class CropType(models.Model):
+    
+    name=models.CharField(max_length=255, unique=True)
+    
+    
+    def __str__(self):
+        return self.name;
+    
+class Farmer(models.Model):
+    
+    user=models.ForeignKey(User, null=True, blank=True,on_delete=models.CASCADE)
+    crops=models.ManyToManyField(CropType)
+    
+    def __str__(self):
+        return self.user.username
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    image = models.ImageField(upload_to='product_images/')
+    farmer = models.ForeignKey(Farmer, on_delete=models.CASCADE)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    buyer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.product.name} - {self.quantity}'
